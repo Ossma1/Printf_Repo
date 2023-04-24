44dd952fac42aa25cecc70512aeee14df4b7c9f7
HARIMECH <sihamharimech00@gmail.com>
2023-04-24 13:28:13 -0700

    task0

--- _printf.c
+++ _printf.c
@@ -3,57 +3,68 @@
 #include <stdio.h>
 
 /**
- * _printf - prints formatted output to stdout
- * @format: A string containing zero or more format specifiers
  *
- * Return: the number of characters printed (excluding the null byte used to end output to strings)
+ * _printf: handle %c and the s and the %
+ * @format: variadic parametr
+ * Return: the number of characters printed
+ * excluding the null byte used to end output to strings
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _printf(const char *format, ...)
 {
-        int count = 0;
-        va_list st;
 
-        va_start(st, format);
-        if (!format)
-        {
-                return (-1);
-        }
+	int count = 0;
+	va_list st;
+	
 
-        while (*format != '\0')
-        {
-                if (*format == '%')
-                {
-                        format++;
-                        if (*format == 'c')
-                        {
-                                char c = (char)va_arg(st, int);
-                                putchar(c);
-                                count++;
-                        }
-                        if (*format == 's')
-                        {
-                                char *str = va_arg(st, char *);
-                                while (*str != '\0')
-                                {
-                                        putchar(*str);
-                                        str++;
-                                        count++;
-                                }
-                        }
-                        if (*format == '%')
-                        {
-                                putchar('%');
-                                count++;
-                        }
-                }
-                else
-                {
-                        putchar(*format);
-                        count++;
-                }
-                format++;
-        }
-        va_end(st);
-       return (count);
+	va_start(st, format);
+
+	if (!format || (*format == '%' && *(format + 1) == '\0'))
+	{
+		return (-1);
+	}
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = (char)va_arg(st, int);
+				putchar(c);
+				
+				count++;
+			}
+
+			if (*format == 's')
+			{
+				char *str = va_arg(st, char *);
+				while (*str != '\0')
+				{
+					putchar(*str);
+					
+					str++;
+					count++;
+				}
+			}
+
+			if (*format == '%')
+			{
+				putchar('%');
+			
+				count++;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			
+			count++;
+		}
+		format++;
+	}
+	va_end(st);
+	return (count);
 }
