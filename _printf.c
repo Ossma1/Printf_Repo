4a7eada3fad1337eea690bd1d81f396acbe6cb1e
HARIMECH <sihamharimech00@gmail.com>
2023-04-24 14:22:19 -0700

    task0

--- _printf.c
+++ _printf.c
@@ -1,49 +1,60 @@
-#include main.h
+#include "main.h"
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
- * _putchar - writes the character c to stdout
- * in this function we gonna handel the option %c
+ * _printf - writes formatted output to stdout
+ * @format: pointer to a format string
  *
  * Return: the number of characters printed (excluding the null byte used to end output to strings)
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int _printf(const char *format, ...){
-	int i=0;
-	int count=0;
+int _printf(const char *format, ...)
+{
+	int count = 0;
 	va_list st;
-	 va_start (st, format);
-	 if (!format){
-	 	return(-1);
-	 }
-	 	while (*format != '\0') {
-	 		if (format[i] == '%')
+	
+	va_start(st, format);
+	if (!format)
+	{
+		return (-1);
+	}
+	
+	while (*format != '\0')
+	{
+		if (*format == '%')
 		{
-			if (format[i + 1] == 'c'){
-				 char c = (char) va_arg(st, int);
-                    putchar(c);
-                    count++;
+			format++;
+			if (*format == 'c')
+			{
+				char c = (char)va_arg(st, int);
+				putchar(c);
+				count++;
 			}
-			if (format[i + 1] == 's'){
+			if (*format == 's')
+			{
 				char *str = va_arg(st, char *);
-                    while (*str != '\0') {
-                        putchar(*str);
-                        str++;
-                        count++;
+				while (*str != '\0')
+				{
+					putchar(*str);
+					str++;
+					count++;
+				}
 			}
-			 }
-			if (format[i + 1] == '%'){
+			if (*format == '%')
+			{
 				putchar('%');
-                    count++;
+				count++;
 			}
-		
-}else {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
-    va_end(st);
-    return count;
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(st);
+	return (count);
 }
