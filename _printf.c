994da2901a96c97e5a825f9828237a368189c5a1
HARIMECH <sihamharimech00@gmail.com>
2023-04-24 14:07:38 -0700

    task0

--- _printf.c
+++ _printf.c
@@ -1,70 +1,49 @@
-#include "main.h"
+#include main.h
 #include <stdarg.h>
-#include <stdio.h>
 
 /**
+ * _putchar - writes the character c to stdout
+ * in this function we gonna handel the option %c
  *
- * _printf: handle %c and the s and the %
- * @format: variadic parametr
- * Return: the number of characters printed
- * excluding the null byte used to end output to strings
+ * Return: the number of characters printed (excluding the null byte used to end output to strings)
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int _printf(const char *format, ...)
-{
-
-	int count = 0;
+int _printf(const char *format, ...){
+	int i=0;
+	int count=0;
 	va_list st;
-	
-
-	va_start(st, format);
-
-	if (!format || (*format == '%' && *(format + 1) == '\0'))
-	{
-		return (-1);
-	}
-
-	while (*format != '\0')
-	{
-		if (*format == '%')
+	 va_start (st, format);
+	 if (!format){
+	 	return(-1);
+	 }
+	 	while (*format != '\0') {
+	 		if (format[i] == '%')
 		{
-			format++;
-			if (*format == 'c')
-			{
-				char c = (char)va_arg(st, int);
-				putchar(c);
-				
-				count++;
+			if (format[i + 1] == 'c'){
+				 char c = (char) va_arg(st, int);
+                    putchar(c);
+                    count++;
 			}
-
-			if (*format == 's')
-			{
+			if (format[i + 1] == 's'){
 				char *str = va_arg(st, char *);
-				while (*str != '\0')
-				{
-					putchar(*str);
-					
-					str++;
-					count++;
-				}
+                    while (*str != '\0') {
+                        putchar(*str);
+                        str++;
+                        count++;
 			}
-
-			if (*format == '%')
-			{
+			 }
+			if (format[i + 1] == '%'){
 				putchar('%');
-			
-				count++;
+                    count++;
 			}
-		}
-		else
-		{
-			putchar(*format);
-			
-			count++;
-		}
-		format++;
-	}
-	va_end(st);
-	return (count);
+		
+}else {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+    va_end(st);
+    return count;
 }
